e3e858ac4ee42d444d7cec5bc7e4d3fdfb5db417
abrmfw <abrmfw@gmail.com>
2026-07-01 15:55:11 -0400

    Add files via upload

---
+++ public/assets/script.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+
+  if (!form) return;
+
+  form.addEventListener("submit", () => {
+    const btn = form.querySelector("button");
+    btn.innerText = "Sending...";
+    btn.disabled = true;
+  });
+});
